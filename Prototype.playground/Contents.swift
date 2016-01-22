b9f658723f4772f544d23668f1c0c7a40ae5f798
Brian Capps <brian@lickability.com>
2016-01-22 16:34:03 -0500

    Proposal for refinements with log collection and initialization (to reduce mutability)

--- Prototype.playground/Contents.swift
+++ Prototype.playground/Contents.swift
@@ -5,14 +5,30 @@ import UIKit
 class PinpointKit {
     
     struct Configuration {
-        static let defaultConfiguration = Configuration()
-
-        var fillColor: UIColor = .redColor()
-        var strokeColor: UIColor = .whiteColor()
         
-        var screenshotEditor: ScreenshotEditor = EditImageViewController()
-        var feedbackCollector: FeedbackCollector = FeedbackViewController()
-        var sender: Sender = MailSender()
+        enum LogCollectionStatus {
+            case CollectSilently(collector: LogCollector)
+            case CollectAndPrompt(collector: LogCollector, promptText: String)
+            case DoNotCollect
+        }
+        
+        let tintColor: UIColor
+        let annotationStrokeColor: UIColor
+        
+        let logCollectionStatus: LogCollectionStatus
+        
+        let screenshotEditor: ScreenshotEditor
+        let feedbackCollector: FeedbackCollector?
+        let sender: Sender
+        
+        init(tintColor: UIColor = .redColor(), annotationStrokeColor: UIColor = .whiteColor(), logCollectionStatus: LogCollectionStatus = LogCollectionStatus.CollectAndPrompt(collector: SystemLogCollector(), promptText: NSLocalizedString("Include Console Log", comment: "Text asking whether or not to include a console log in a feedback form.")), screenshotEditor: ScreenshotEditor = EditImageViewController(), feedbackCollector: FeedbackCollector? = FeedbackViewController(), sender: Sender = MailSender()) {
+            self.tintColor = tintColor
+            self.annotationStrokeColor = annotationStrokeColor
+            self.logCollectionStatus = logCollectionStatus
+            self.screenshotEditor = screenshotEditor
+            self.feedbackCollector = feedbackCollector
+            self.sender = sender
+        }
     }
     
     static let defaultPinpointKit = PinpointKit()
@@ -20,7 +36,7 @@ class PinpointKit {
     let configuration: Configuration
     weak var delegate: PinpointKitDelegate?
     
-    init(configuration: Configuration = Configuration.defaultConfiguration, delegate: PinpointKitDelegate? = nil)  {
+    init(configuration: Configuration = Configuration(), delegate: PinpointKitDelegate? = nil)  {
         self.configuration = configuration
         self.delegate = delegate
     }
@@ -41,7 +57,23 @@ protocol PinpointKitDelegate: class {
     func pinpointKit(pinpointKit: PinpointKit, didFinishWithResult: Bool)
 }
 
+// Default protocol extension to make all functions in the delegate optional.
+extension PinpointKitDelegate {
+    func pinpointKit(pinpointKit: PinpointKit, willShowFeedbackCollector: FeedbackCollector) {}
+    func pinpointKit(pinpointKit: PinpointKit, didShowFeedbackCollector: FeedbackCollector) {}
+    
+    func pinpointKit(pinpointKit: PinpointKit, willSendFeedback: Feedback) {}
+    func pinpointKit(pinpointKit: PinpointKit, didSendFeedback: Feedback) {}
+    
+    func pinpointKit(pinpointKit: PinpointKit, willShowScreenshotEditor: ScreenshotEditor) {}
+    func pinpointKit(pinpointKit: PinpointKit, didShowScreenshotEditor: ScreenshotEditor) {}
+    
+    func pinpointKit(pinpointKit: PinpointKit, didFinishWithResult: Bool) {}
+}
+
 protocol Sender {}
+
+protocol LogCollector {}
 
 struct Feedback {
 
@@ -70,5 +102,8 @@ class EditImageViewController: UIViewController, ScreenshotEditor {}
 
 class FeedbackViewController: UIViewController, FeedbackCollector {}
 
-class MailSender: NSObject, Sender {}
+class MailSender: Sender {}
+
+class SystemLogCollector: LogCollector {}
+
 
