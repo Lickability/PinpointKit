571a2afd2fc35d86f7ccc4c58936000f7f82f576
Paul Rehkugler <paul@tumblr.com>
2016-01-22 19:48:22 -0500

    File > New Project... > iOS Framework & Library > Cocoa Touch Framework

---
+++ PinpointKit/PinpointKit/PinpointKit.h
@@ -0,0 +1,19 @@
+//
+//  PinpointKit.h
+//  PinpointKit
+//
+//  Created by Paul Rehkugler on 1/22/16.
+//  Copyright © 2016 Lickability. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PinpointKit.
+FOUNDATION_EXPORT double PinpointKitVersionNumber;
+
+//! Project version string for PinpointKit.
+FOUNDATION_EXPORT const unsigned char PinpointKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PinpointKit/PublicHeader.h>
+
+
---
+++ PinpointKit/PinpointKitTests/PinpointKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  PinpointKitTests.swift
+//  PinpointKitTests
+//
+//  Created by Paul Rehkugler on 1/22/16.
+//  Copyright © 2016 Lickability. All rights reserved.
+//
+
+import XCTest
+@testable import PinpointKit
+
+class PinpointKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
