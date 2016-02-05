6871aa291c0419bfdea6b585dd61fc93976d43cb
Brian Capps <brian@lickability.com>
2016-02-05 14:48:09 -0500

    Adds skeleton concrete objects for the protocols

---
+++ PinpointKit/PinpointKit/BasicLogViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BasicLogViewController.swift
+//  PinpointKit
+//
+//  Created by Brian Capps on 2/5/16.
+//  Copyright © 2016 Lickability. All rights reserved.
+//
+
+import UIKit
+
+class BasicLogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PinpointKit/PinpointKit/MailSender.swift
@@ -0,0 +1,14 @@
+//
+//  MailSender.swift
+//  PinpointKit
+//
+//  Created by Brian Capps on 2/5/16.
+//  Copyright © 2016 Lickability. All rights reserved.
+//
+
+import MessageUI
+
+class MailSender: NSObject, MFMailComposeViewControllerDelegate, Sender {
+    
+}
+
