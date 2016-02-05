424e71afa5e90fa6e465cfcffae11d7d22b18960
Brian Capps <brian@lickability.com>
2016-02-05 14:36:22 -0500

    Adds skeleton types for Configuration and protocols

---
+++ PinpointKit/PinpointKit/Configuration.swift
@@ -0,0 +1,16 @@
+//
+//  Configuration.swift
+//  PinpointKit
+//
+//  Created by Brian Capps on 2/5/16.
+//  Copyright © 2016 Lickability. All rights reserved.
+//
+
+import Foundation
+
+/**
+ *  A struct that contains configuration information for the behavior and appearance of PinpointKit.
+ */
+struct Configuration {
+    
+}
