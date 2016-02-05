b93e91dabda270046b0ac08dd8cb6ea407462c86
Brian Capps <brian@lickability.com>
2016-02-05 14:51:06 -0500

    Adds properties and Appearance struct to configuration

--- PinpointKit/PinpointKit/Configuration.swift
+++ PinpointKit/PinpointKit/Configuration.swift
@@ -13,4 +13,40 @@ import Foundation
  */
 struct Configuration {
     
+    struct Appearance {
+        
+        // The fill color for annotations. If none is supplied, the `tintColor` of the relevant view will be used.
+        let annotationFillColor: UIColor?
+        
+        /// The stroke color for annotations.
+        let annotationStrokeColor: UIColor
+        
+        /**
+         Initializes an `Appearance` object with a optional annotation color properties.
+         
+         - parameter annotationFillColor:   The fill color for annotations. If none is supplied, the `tintColor` of the relevant view will be used.
+         - parameter annotationStrokeColor: The stroke color for annotations.
+         
+         - returns: A fully initialized `Appearance` object.
+         */
+        init(annotationFillColor: UIColor? = nil, annotationStrokeColor: UIColor = .whiteColor()) {
+            self.annotationFillColor = annotationFillColor
+            self.annotationStrokeColor = annotationStrokeColor
+        }
+    }
+    
+    let appearance: Appearance
+    
+    let logCollector: LogCollector?
+    let feedbackCollector: FeedbackCollector
+    let editor: Editor
+    let sender: Sender
+    
+    init(appearance: Appearance = Appearance(), logCollector: LogCollector? = SystemLogCollector(), feedbackCollector: FeedbackCollector = FeedbackViewController(), editor: Editor = EditImageViewController(), sender: Sender = MailSender()) {
+        self.appearance = appearance
+        self.logCollector = logCollector
+        self.feedbackCollector = feedbackCollector
+        self.editor = editor
+        self.sender = sender
+    }
 }
