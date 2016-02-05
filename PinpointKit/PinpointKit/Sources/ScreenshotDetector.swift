e36fee701c1caa0ede68be71731f8cb04332109a
Matthew Bischoff <mb@matthewbischoff.com>
2016-02-05 15:54:15 -0500

    Lots of progress on screenshot detection and error states

--- PinpointKit/PinpointKit/Sources/ScreenshotDetector.swift
+++ PinpointKit/PinpointKit/Sources/ScreenshotDetector.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import Photos
 
+/// A class that detects when the user has taken a screenshot and provides it via a delegate callback.
 final class ScreenshotDetector: NSObject {
     private let notificationCenter: NSNotificationCenter
     private let application: UIApplication
+    private let imageManager: PHImageManager
+    
+    enum Error: ErrorType {
+        case Unauthorized(status: PHAuthorizationStatus)
+        case FetchFailure
+        case LoadFailure
+    }
 
-    init(pinpointKit: PinpointKit, notificationCenter: NSNotificationCenter = NSNotificationCenter.defaultCenter(), application: UIApplication = UIApplication.sharedApplication()) {
+    init(notificationCenter: NSNotificationCenter = .defaultCenter(), application: UIApplication = .sharedApplication(), imageManager: PHImageManager = .defaultManager()) {
         self.notificationCenter = notificationCenter
         self.application = application
+        self.imageManager = imageManager
         
         super.init()
         
@@ -22,7 +32,53 @@ final class ScreenshotDetector: NSObject {
     }
     
     private func userTookScreenshot(notification: NSNotification) {
+        
+        requestPhotosAuthorization()
+        
+        //TODO: Open PinpointKit.
+    }
+    
+    private func requestPhotosAuthorization() {
+        PHPhotoLibrary.requestAuthorization { authorizationStatus in
+            switch authorizationStatus {
+            case .Authorized:
+                self.findScreenshot()
+            case .Denied, .NotDetermined, .Restricted:
+                self.fail()
+            }
+        }
+    }
+    
+    private func findScreenshot() {
+        guard let screenshot = PHFetchResult.lastScreenshotFetchResult().firstObject as? PHAsset else {
+            fail()
+            return }
+
+        imageManager.requestImageForAsset(screenshot, targetSize: PHImageManagerMaximumSize, contentMode: PHImageContentMode.Default, options: nil) { (image, info) -> Void in
+            
+        }
+    }
+    
+    private func fail() {
     
     }
+}
 
+protocol ScreenshotDetectorDelegate: class {
+    func screenshotDetector(screenshotDetector: ScreenshotDetector, didDetectScreenshot: UIImage)
+    func screenshotDetector(screenshotDetector: ScreenshotDetector, didFailWithError: ScreenshotDetector.Error)
+}
+
+private extension PHFetchResult {
+    static func lastScreenshotFetchResult() -> PHFetchResult {
+        let options = PHFetchOptions()
+        
+        options.fetchLimit = 1
+        options.includeAssetSourceTypes = [.TypeUserLibrary]
+        options.wantsIncrementalChangeDetails = false
+        options.predicate = NSPredicate(format: "(mediaSubtype & %d) != 0", PHAssetMediaSubtype.PhotoScreenshot.rawValue)
+        options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        
+        return PHAsset.fetchAssetsWithMediaType(.Image, options: options)
+    }
 }
