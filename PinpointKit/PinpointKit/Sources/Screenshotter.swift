fdd2f59ff89a666ea26fd8c8dce614b6d82ece57
Matthew Bischoff <mb@matthewbischoff.com>
2016-02-19 16:20:03 -0500

    Added a Screenshotter

---
+++ PinpointKit/PinpointKit/Sources/Screenshotter.swift
@@ -0,0 +1,25 @@
+//
+//  Screenshotter.swift
+//  PinpointKit
+//
+//  Created by Matthew Bischoff on 2/19/16.
+//  Copyright © 2016 Lickability. All rights reserved.
+//
+
+import Foundation
+
+public class Screenshotter {
+
+    public class func takeScreenshot(screen: UIScreen = UIScreen.mainScreen(), application: UIApplication = UIApplication.sharedApplication()) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(screen.bounds.size, true, 0)
+        
+        application.windows.forEach { window in
+            window.drawViewHierarchyInRect(window.bounds, afterScreenUpdates: false)
+        }
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+}
