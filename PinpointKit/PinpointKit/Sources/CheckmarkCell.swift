10060082e520426766273dadf194b1fb1d686de8
Matthew Bischoff <mb@matthewbischoff.com>
2016-03-18 15:00:18 -0400

    pod installed after adding CheckmarkCell.

---
+++ PinpointKit/PinpointKit/Sources/CheckmarkCell.swift
@@ -0,0 +1,17 @@
+//
+//  CheckmarkCell.swift
+//  PinpointKit
+//
+//  Created by Matthew Bischoff on 3/18/16.
+//  Copyright © 2016 Lickability. All rights reserved.
+//
+
+import UIKit
+
+final class CheckmarkCell: UITableViewCell {
+    var isChecked: Bool = false {
+        didSet {
+            imageView?.image = isChecked ? UIImage(named: "checkmark") : nil
+        }
+    }
+}
