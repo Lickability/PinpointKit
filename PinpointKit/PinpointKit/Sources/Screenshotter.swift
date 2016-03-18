a0205f347132c0e6b1de42c9ff14247aa6a7e4f3
Matthew Bischoff <mb@matthewbischoff.com>
2016-03-18 15:13:05 -0400

    Added documentation to Screenshotter.

--- PinpointKit/PinpointKit/Sources/Screenshotter.swift
+++ PinpointKit/PinpointKit/Sources/Screenshotter.swift
@@ -8,12 +8,23 @@
 
 import Foundation
 
+/// A class responsible for generating a screenshot image of all windows shown by a `UIApplication` on a given `UIScreen`.
 public class Screenshotter {
 
+    /**
+     Takes and returns a screenshot of all of an `application`’s windows displayed on a given screen.
+     
+     - parameter screen:      The screen to determine the screenshot size.
+     - parameter application: The application to screenshot.
+     
+     - returns: A screenshot as a `UIImage`.
+     */
     public class func takeScreenshot(screen: UIScreen = UIScreen.mainScreen(), application: UIApplication = UIApplication.sharedApplication()) -> UIImage {
         UIGraphicsBeginImageContextWithOptions(screen.bounds.size, true, 0)
         
         application.windows.forEach { window in
+            guard window.screen == screen else { return }
+            
             window.drawViewHierarchyInRect(window.bounds, afterScreenUpdates: false)
         }
         
