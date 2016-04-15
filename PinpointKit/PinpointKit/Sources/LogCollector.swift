8ae330f0e5f616c6269836d9a3b55585ed784325
Brian Capps <brian@lickability.com>
2016-04-15 16:38:51 -0400

    log collector and system log collector docs

--- PinpointKit/PinpointKit/Sources/LogCollector.swift
+++ PinpointKit/PinpointKit/Sources/LogCollector.swift
@@ -6,6 +6,13 @@
 //  Copyright © 2016 Lickability. All rights reserved.
 //
 
+/// A behavior protocol that describes an object that collects logs.
 public protocol LogCollector {
     
+    /**
+     Retrieves and returns logs as an ordered list of strings.
+     
+     - returns: Logs as an ordered list of strings.
+     */
+    func retrieveLogs() -> [String]
 }
--- PinpointKit/PinpointKit/Sources/SystemLogCollector.swift
+++ PinpointKit/PinpointKit/Sources/SystemLogCollector.swift
@@ -6,12 +6,20 @@
 //  Copyright © 2016 Lickability. All rights reserved.
 //
 
+/// A log collector that uses [Apple System Logger](https://developer.apple.com/library/mac/documentation/MacOSX/Conceptual/BPSystemStartup/Chapters/LoggingErrorsAndWarnings.html) API to retrieve messages logged to the console with `NSLog`.
 public class SystemLogCollector: LogCollector {
     
     private let logger = ASLLogger()
     
     public init() { }
     
+    // MARK: - LogCollector
+    
+    /**
+     Retrieves and returns logs as an ordered list of strings.
+     
+     - returns: Logs as an ordered list of strings.
+     */
     public func retrieveLogs() -> [String] {
         return logger.retrieveLogs()
     }
