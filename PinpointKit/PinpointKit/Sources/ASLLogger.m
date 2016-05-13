ef80a4647ef62d4c6877629fc816c65cd9e0b252
twigz <aharris.1990@gmail.com>
2016-05-13 15:24:04 -0400

    Modified string display

--- PinpointKit/PinpointKit/Sources/ASLLogger.m
+++ PinpointKit/PinpointKit/Sources/ASLLogger.m
@@ -14,14 +14,10 @@
 - (NSArray<NSString *> *)retrieveLogs {
     NSMutableArray<NSString *> *logs = [NSMutableArray array];
     
-    long lastTime = 0;
-    
     aslmsg query = NULL, message = NULL;
     aslresponse response = NULL;
     
     query = asl_new(ASL_TYPE_QUERY);
-    const char *time = [[NSString stringWithFormat:@"%ld", lastTime] UTF8String];
-    asl_set_query(query, ASL_KEY_TIME, time, ASL_QUERY_OP_GREATER | ASL_QUERY_OP_NUMERIC);
     asl_set_query(query, ASL_KEY_FACILITY, [[[NSBundle mainBundle] bundleIdentifier] UTF8String], ASL_QUERY_OP_EQUAL);
     
     response = asl_search(NULL, query);
@@ -35,16 +31,27 @@
         }
         
         const char *content = asl_get(message, ASL_KEY_MSG);
-        const char *time = asl_get(message, ASL_KEY_TIME);
-        lastTime = atoi(time);
+        NSTimeInterval msgTime = (NSTimeInterval) atol(asl_get(message, ASL_KEY_TIME)) + ((NSTimeInterval) atol(asl_get(message, ASL_KEY_TIME_NSEC)) / 1000000000.0);
         
-        [logs addObject:[[NSString alloc] initWithUTF8String:content]];
+        NSString *contentString = [[NSString alloc] initWithUTF8String:content];
+        NSString *timeString = [self stringFromTimeInterval:msgTime];
+        NSString *loggedText = [NSString stringWithFormat:@"%@ %@", timeString, contentString];
+        
+        [logs addObject:loggedText];
     }
     
     asl_release(response);
     asl_free(query);
     
     return logs;
+}
+
+- (NSString *)stringFromTimeInterval:(NSTimeInterval)interval {
+    NSInteger ti = (NSInteger)interval;
+    NSInteger seconds = ti % 60;
+    NSInteger minutes = (ti / 60) % 60;
+    NSInteger hours = (ti / 3600);
+    return [NSString stringWithFormat:@"%02ld:%02ld:%02ld", (long)hours, (long)minutes, (long)seconds];
 }
 
 @end
