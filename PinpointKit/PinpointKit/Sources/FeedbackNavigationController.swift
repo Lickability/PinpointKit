a91b89b06e05cef98bebe9519b744369974fd75a
kenny <pearapps@gmail.com>
2016-05-15 19:08:42 -0400

    This class was not designed to be subclasses

--- PinpointKit/PinpointKit/Sources/FeedbackNavigationController.swift
+++ PinpointKit/PinpointKit/Sources/FeedbackNavigationController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 /// A `UINavigationController` subclass that has a `FeedbackViewController` as its root view controller. Use this class as a `FeedbackCollector`.
-public class FeedbackNavigationController: UINavigationController, FeedbackCollector {
+public final class FeedbackNavigationController: UINavigationController, FeedbackCollector {
     
     // MARK: - InterfaceCustomization
     
