2a71ba00876e523565b7741200f217412c9ffc3b
Michael Liberatore <mi@chael.co>
2016-05-20 13:09:46 -0400

    Updates docs on `editorWillDismiss(_:screenshot:)`

--- PinpointKit/PinpointKit/Sources/Editing/EditorDelegate.swift
+++ PinpointKit/PinpointKit/Sources/Editing/EditorDelegate.swift
@@ -2,7 +2,7 @@
 public protocol EditorDelegate: class {
     
     /**
-     A function that is called with an image when the view controller begins dismissing.
+     A function that is called with an image just before the editor is dismissed.
      
      - parameter editor: The editor resonsible for editing the image.
      - parameter screenshot: The edited image of a screenshot, after editing is complete.
