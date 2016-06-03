8e108efbda6243c69c9263312b5eeaa36b0e32bc
Michael Liberatore <mi@chael.co>
2016-06-03 15:01:34 -0400

    Adds comment explaining subview iteration

--- PinpointKit/PinpointKit/Sources/AnnotationsView.swift
+++ PinpointKit/PinpointKit/Sources/AnnotationsView.swift
@@ -40,6 +40,10 @@ class AnnotationsView: UIView {
     override func tintColorDidChange() {
         super.tintColorDidChange()
         
+        // In certain cases, subviews would otherwise revert to the system default `tintColor`.
+        // One such case occurs when an alert controller is shown followed by dismissing and re-presenting
+        // a view controller containing an `AnnotationsView` when the `AnnotationsView` has a custom 
+        // (non-inherited) `tintColor`.
         for subview in subviews {
             subview.tintColor = tintColor
         }
