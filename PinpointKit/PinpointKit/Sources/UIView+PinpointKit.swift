000cdd321fa0b92cfce141d365991cc816407cae
twigz <aharris.1990@gmail.com>
2016-06-10 15:10:27 -0400

    Rewords snapshotting to specify view instead of screen

--- PinpointKit/PinpointKit/Sources/UIView+PinpointKit.swift
+++ PinpointKit/PinpointKit/Sources/UIView+PinpointKit.swift
@@ -9,7 +9,7 @@
 /// Extends UIView to take a snapshot of the screen.
 extension UIView {
     
-    /// The UIImage representation of the current screen at the time of access.
+    /// The UIImage representation of this view at the time of access.
     var pinpoint_screenshot: UIImage {
         UIGraphicsBeginImageContextWithOptions(bounds.size, true, 0)
         drawViewHierarchyInRect(bounds, afterScreenUpdates: true)
