56a9f5e0e5e337c4457773873eb7a697ad60cc9a
Michael Liberatore <mi@chael.co>
2016-06-10 13:47:19 -0400

    Adds docs to `BarButtonItem.swift`

--- PinpointKit/PinpointKit/Sources/BarButtonItem.swift
+++ PinpointKit/PinpointKit/Sources/BarButtonItem.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 extension UIBarButtonItem {
+    
+    /**
+     Convenience initializer for creating a “Done” `UIBarButtonItem` with a specified target, action, and font.
+     
+     - parameter target: The bar button item’s target.
+     - parameter font:   The font of the bar button item’s title.
+     - parameter action: The bar button item’s action.
+     
+     - returns: An initialized “Done” `UIBarButtonItem`.
+     */
     convenience init(doneButtonWithTarget target: AnyObject?, font: UIFont, action: Selector) {
         self.init(barButtonSystemItem: .Done, target: target, action: action)
         
