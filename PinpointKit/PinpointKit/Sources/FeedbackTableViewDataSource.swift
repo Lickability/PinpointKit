27bce32e6e0398cf20808487326ded3c32923489
Michael Liberatore <mi@chael.co>
2016-06-10 14:13:00 -0400

    Makes adjustments to docs on `FeedbackTableViewDataSource`

--- PinpointKit/PinpointKit/Sources/FeedbackTableViewDataSource.swift
+++ PinpointKit/PinpointKit/Sources/FeedbackTableViewDataSource.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-/// An object conforming to `UITableViewDataSource` that acts as the data souce for a `FeedbackViewController`.
+/// An object conforming to `UITableViewDataSource` that acts as the data source for a `FeedbackViewController`.
 final class FeedbackTableViewDataSource: NSObject, UITableViewDataSource {
     
     private let sections: [Section]
@@ -16,9 +16,11 @@ final class FeedbackTableViewDataSource: NSObject, UITableViewDataSource {
     /**
      Initializes the data source with a configuration and a boolean value indicating whether the user has enabled log collection.
      
-     - parameter interfaceCustomization:            The configuration used to set up the data source.
+     - parameter interfaceCustomization:   The configuration used to set up the data source.
      - parameter logSupporting:            The object the controls the support of logging.
-     - parameter userEnabledLogCollection:  A boolean value indicating whether the user has enabled log collection.
+     - parameter userEnabledLogCollection: A boolean value indicating whether the user has enabled log collection.
+     
+     - returns: An initialized `FeedbackTableViewDataSource`.
      */
     init(interfaceCustomization: InterfaceCustomization, logSupporting: LogSupporting, userEnabledLogCollection: Bool) {
         sections = self.dynamicType.sectionsFromConfiguration(interfaceCustomization, logSupporting: logSupporting, userEnabledLogCollection: userEnabledLogCollection)
