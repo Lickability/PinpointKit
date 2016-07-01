c34451b88746c3662c63b5bfd95c54315da731e5
Michael Liberatore <mi@chael.co>
2016-07-01 14:32:42 -0400

    Removes use of `ShakeDetectingWindow`
    See https://github.com/Lickability/PinpointKit/pull/130#issuecomment-230016495

--- Example/PinpointKitExample/AppDelegate.swift
+++ Example/PinpointKitExample/AppDelegate.swift
@@ -11,9 +11,8 @@ import PinpointKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    private let pinpointKit = PinpointKit(feedbackRecipients: ["feedback@example.com"])
-    lazy var window: UIWindow? = ShakeDetectingWindow(frame: UIScreen.mainScreen().bounds, delegate: self.pinpointKit)
+    
+    var window: UIWindow?
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         NSLog("Initial test log for the system logger.")
