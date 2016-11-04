7cfd4079b3ed51a7dd99140f22903ea773da770d
Michael Liberatore <mi@chael.co>
2016-11-04 18:02:40 -0400

    Adds test code for custom bar button items

--- Example/PinpointKitExample/ViewController.swift
+++ Example/PinpointKitExample/ViewController.swift
@@ -11,7 +11,19 @@ import PinpointKit
 
 final class ViewController: UITableViewController {
     
-    fileprivate let pinpointKit = PinpointKit(feedbackRecipients: ["feedback@example.com"])
+    fileprivate let pinpointKit = PinpointKit(configuration: Configuration(editor: {
+        
+        // Create a custom editor
+        let editImageViewController = EditImageViewController()
+        
+        
+        // Associate that custom bar button item provider with the ditor.
+        editImageViewController.barButtonItemProvider = MyButtonProvider(editImageViewController: editImageViewController)
+        
+        return editImageViewController
+        
+        }(), feedbackConfiguration: FeedbackConfiguration(recipients: ["example@something.com"]))
+    )
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,5 +36,29 @@ final class ViewController: UITableViewController {
         super.viewDidAppear(animated)
         
         pinpointKit.show(from: self)
+    }
+}
+
+// Create a custom bar button item provider class
+class MyButtonProvider: EditImageViewControllerBarButtonItemProviding {
+    private weak var editImageViewController: EditImageViewController?
+    
+    init(editImageViewController: EditImageViewController) {
+        self.editImageViewController = editImageViewController
+    }
+    
+    lazy var leftBarButtonItem: UIBarButtonItem? = UIBarButtonItem(title: "CLOSE!", style: .done, target: self, action: #selector(MyButtonProvider.close(sender:)))
+    
+    
+    lazy var rightBarButtonItem: UIBarButtonItem? = UIBarButtonItem(title: "LOG!", style: .plain, target: self, action: #selector(MyButtonProvider.logSomething(sender:)))
+    
+    let allowsHidingBarButtonItemsWhileEditingTextAnnotations: Bool = true
+    
+    @objc private func close(sender: UIBarButtonItem) {
+        self.editImageViewController?.attemptToDismiss(animated: true)
+    }
+    
+    @objc private func logSomething(sender: UIBarButtonItem) {
+        print("HEY")
     }
 }
