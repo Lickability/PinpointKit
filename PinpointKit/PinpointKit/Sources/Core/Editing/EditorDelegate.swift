d636e1c56d8d6aaccf9cd38b5c78ab9919f96a7f
Michael Liberatore <mi@chael.co>
2016-12-09 14:09:27 -0500

    Adds more granular editor change delegate

--- PinpointKit/PinpointKit/Sources/Core/Editing/EditorDelegate.swift
+++ PinpointKit/PinpointKit/Sources/Core/Editing/EditorDelegate.swift
@@ -23,11 +23,12 @@ public protocol EditorDelegate: class {
      A method that is called any time the editor makes a modification to the screenshot.
      
      - parameter editor: The editor resonsible for editing the image.
+     - parameter change: The change that was made to the screenshot.
      - parameter screenshot: The edited image of a screenshot.
      
      - note: The default implementation of this method does nothing.
      */
-    func editorDidMakeChange(_ editor: Editor, to screenshot: UIImage)
+    func editor(_ editor: Editor, didMake change: AnnotationChange, to screenshot: UIImage)
     
     /**
      A method that is called with an image to ask if the editor should be dismissed.
@@ -67,7 +68,7 @@ extension EditorDelegate {
         // Do nothing
     }
     
-    public func editorDidMakeChange(_ editor: Editor, to screenshot: UIImage) {
+    public func editor(_ editor: Editor, didMake change: AnnotationChange, to screenshot: UIImage) {
         // Do nothing
     }
     
@@ -82,4 +83,26 @@ extension EditorDelegate {
     public func editorDidDismiss(_ editor: Editor, with screenshot: UIImage) {
         // Do nothing
     }
+}
+
+/// Represents a change made using the editor.
+public enum AnnotationChange {
+    
+    /// An annotation was added.
+    case added
+    
+    /// An annotation was moved.
+    case moved
+    
+    /// An annotation was brought to front.
+    case broughtToFront
+    
+    /// An annotation was resized.
+    case resized
+    
+    /// A text annotation was edited.
+    case textEdited
+    
+    /// An annotation was deleted. `animated` represents whether the deletion is animated.
+    case deleted(animated: Bool)
 }
