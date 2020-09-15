9eead2fc08c48ccb25a9f5145ae01f069c6b892e
Michael Liberatore <mi@chael.co>
2020-09-15 12:18:35 -0400

    Makes use of ScreenshotDetector

--- Example/PinpointKitExample/ViewController.swift
+++ Example/PinpointKitExample/ViewController.swift
@@ -9,20 +9,29 @@
 import UIKit
 import PinpointKit
 
-final class ViewController: UITableViewController {
+final class ViewController: UITableViewController, ScreenshotDetectorDelegate {
+
+    private var screenshotDetector: ScreenshotDetector?
     
     fileprivate let pinpointKit = PinpointKit(feedbackRecipients: ["feedback@example.com"])
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        screenshotDetector = ScreenshotDetector(delegate: self)
+        
         // Hides the infinite cells footer.
         tableView.tableFooterView = UIView()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        pinpointKit.show(from: self)
+    // MARK: - ScreenshotDetectorDelegate
+    
+    func screenshotDetector(_ screenshotDetector: ScreenshotDetector, didDetect screenshot: UIImage) {
+        pinpointKit.show(from: self, screenshot: screenshot)
     }
+    
+    func screenshotDetector(_ screenshotDetector: ScreenshotDetector, didFailWith error: ScreenshotDetector.Error) {
+        print(error)
+    }
+    
 }
